247c943ee3475e4e44888936c16c6878f336408f
Amber Mohler <ambermohler@ambers-imac.lan>
2020-06-08 23:04:01 -0400

    completed stack.py

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,60 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# class Stack:
+#     def __init__(self):
+# not needed with array? --> self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.storage.append(value)
+
+#     def pop(self):
+#         if len(self.storage) > 0:
+#             return self.storage.pop()
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_tail(value)
+        return self.storage.tail.value
 
     def pop(self):
-        pass
+        if self.storage.tail != None:
+            self.size -= 1
+            return self.storage.remove_tail()
+       
+            
+
+my_stack = Stack() 
+print(my_stack.push('first'))
+print(len(my_stack))
+print(my_stack.push('second'))
+print(len(my_stack))
+print(my_stack.push('third'))
+print(len(my_stack))
+print(my_stack.pop())
+print(len(my_stack))
+print(my_stack.pop())
+print(len(my_stack))
+print(my_stack.pop())
+print(len(my_stack))
+print(my_stack.pop())
+print(len(my_stack))
+print(len(my_stack))
+print(my_stack.pop())
+print(my_stack.pop())
+print(my_stack.pop())
+
+
+
